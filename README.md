ca45c639574f0cdeb2d1069b3b7c9f35e7b3d301
domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2024-06-15 18:37:13 +0200

    Add npx support

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # Lighthouse Crawler
 
+## Development
+
+Once the setup is complete, you can execute the command below in the root
+directory of your project.
+
+```bash
+npx lighthouse-crawler
+```
+
+Make sure you have npm (Node Package Manager) and Node.js installed on
+your system before running this command. Additionally, run `npm install` in
+the project's root directory to install all required dependencies.
+
 ## Linting
 
 To maintain code quality and adhere to coding standards, you can use linting
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+#!/usr/bin/env node
+
 import { Crawler } from './modules/crawler/crawler.js';
+import { hideBin } from 'yargs/helpers';
+import yargs from 'yargs/yargs';
 
-// Convert this to a parameter
-const START_URL = 'http://localhost:8000/';
+const { start } = await yargs(hideBin(process.argv))
+  .option('start', {
+    alias: 's',
+    default: 'http://localhost:80/',
+    description: 'The URL to start with',
+    type: 'string'
+  })
+  .help()
+  .alias('help', 'h')
+  .alias('version', 'v')
+  .argv;
 
-const crawler = new Crawler(START_URL);
+const crawler = new Crawler(start);
 await crawler.crawl();
 console.warn(crawler.results);
